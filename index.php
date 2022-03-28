8ffe81bccd7f4f584c5a8106c6af27f43c92ed87
AlanGio <alan.gio@gmail.com>
2022-03-28 13:21:12 -0300

    Hello world php

---
+++ index.php
@@ -0,0 +1,102 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+  <meta charset="utf-8" />
+  <meta http-equiv="x-ua-compatible"
+        content="ie=edge" />
+  <meta name="viewport"
+        content="width=device-width, initial-scale=1" />
+
+  <title>Noche de Rol</title>
+  <link rel="stylesheet"
+        href="https://cdn.jsdelivr.net/npm/bootstrap-icons@1.8.1/font/bootstrap-icons.css">
+
+
+  <link rel="icon"
+        href="images/favicon.png" />
+  <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/css/bootstrap.min.css"
+        rel="stylesheet"
+        integrity="sha384-1BmE4kWBq78iYhFldvKuhfTAU6auU8tT94WrHftjDbrCEXSU1oBoqyl2QvZ6jIW3"
+        crossorigin="anonymous">
+
+  <style>
+  body {
+    background-color: #2d3030;
+    height: 100vh;
+  }
+
+  .container {
+    height: 50%;
+    padding-top: 5%;
+  }
+
+  .row {
+    height: 100%;
+
+  }
+
+  .background {
+    background-image: url(images/bg.png);
+    background-size: contain;
+    background-repeat: no-repeat;
+    background-position: center;
+  }
+
+  h4 {
+    color: #ccc;
+    margin-bottom: 16px;
+  }
+
+  i {
+    font-size: 40px;
+    margin: 0 8px;
+    color: #fff;
+  }
+
+  a {
+    text-decoration: none;
+  }
+
+
+  a:hover {
+    opacity: 0.7;
+  }
+  </style>
+
+</head>
+
+<body>
+
+  <div class="container">
+    <div class="row background">
+
+    </div>
+  </div>
+  <div class="container">
+    <div class="col title text-center">
+      <h4>En vivo todos los jueves</h4>
+    </div>
+    <div class="col title text-center">
+      <a href="https://www.twitch.tv/nochederol">
+        <i class="bi bi-twitch"></i>
+      </a>
+      <a href="https://www.facebook.com/Nochederol">
+        <i class="bi bi-facebook"></i>
+      </a>
+      <a href="https://www.instagram.com/nochederol/">
+        <i class="bi bi-twitter"></i>
+      </a>
+    </div>
+  </div>
+
+
+
+  <script src="js/scripts.js"></script>
+  <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/js/bootstrap.bundle.min.js"
+          integrity="sha384-ka7Sk0Gln4gmtz2MlQnikT1wXgYsOg+OMhuP+IlRH9sENBO0LRn5q+8nbTov4+1p"
+          crossorigin="anonymous"></script>
+
+</body>
+
+</html>
